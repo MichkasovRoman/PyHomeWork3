1f6f6f2fea28c49bacbdedf77f71a63587375454
Michkasov Roman <mihjob@list.ru>
2022-12-21 23:38:34 +0300

    task18

--- task18.py
+++ task18.py
@@ -12,18 +12,40 @@
 # Вывод: 6
 
 n = int(input('Введите длину списка: '))
-if n <= 0:
-    print('Недопустимый формат числа.')
+print('Получившийся массив: ')
+import random      
+randomList = []
+for elements in range(0, n):
+    randomList.append(random.randint(1, n))
+print(randomList)
+
+print('')
+
+x = int(input('Введите число, которое необходимо проверить: '))
+
+randomList.append(x)
+setArr = set(randomList)   # таким нехитрым способом я удаляю из списка повторяющиеся элементы   
+listSet = list(setArr)     # и упорядочиваю элементы нового списка по возрастанию  
+# print(listSet)     
+lastInd = len(listSet) - 1
+
+if listSet[0] == x:
+    print(listSet[1])
+elif listSet[lastInd] == x:
+    print(listSet[lastInd - 1])
 else:
-    print('Получившийся массив: ')
-    import random      
-    randomList = []
-    for elements in range(1, n + 1):
-        randomList.append(random.randint(0, n))
-    print(randomList)
-    print('')
+    i = 1
+    temp1 = 0
+    temp2 = 0
+    while i <= lastInd - 1:
+        if listSet[i] == x:
+            temp1 = listSet[i - 1]
+            temp2 = listSet[i + 1]
+        i = i + 1
 
-    # x = int(input('Введите число, которое необходимо проверить: '))
+    if x - temp1 <= temp2 - x:
+        print(f'Элемент списка, ближайший к числу {x}:\n{temp1}')
+    else:
+        print(f'Элемент списка, ближайший к числу {x}:\n{temp2}') 
 
-    print(randomList.sort())
     
