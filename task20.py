6a06f99dfd55b9cb969f44048060c75eda3dc5e0
Michkasov Roman <mihjob@list.ru>
2022-12-23 17:18:21 +0300

    task20 last commit

--- task20.py
+++ task20.py
@@ -27,47 +27,40 @@
 
 lettersDictionary = \
     {
-        1: 'АВЕИНОРСТAEIOULNSTR', 
-        2: 'ДКЛМПУDG', 
-        3: 'БГЁЬЯBCMP', 
-        4: 'ЙЫFHVWY', 
-        5: 'ЖЗХЦЧK', 
-        8: 'ШЭЮJX', 
-        10: 'ФЩЪQZ'
+        'АВЕИНОРСТAEIOULNSTR': 1, 
+        'ДКЛМПУDG': 2, 
+        'БГЁЬЯBCMP': 3, 
+        'ЙЫFHVWY': 4, 
+        'ЖЗХЦЧK': 5, 
+        'ШЭЮJX': 6, 
+        'ФЩЪQZ': 7
     }
 
 def WordOrNot(string):             # метод проверяет, является ли введенная пользователем строка словом
     stringList = list(string)
-    for elements in stringList:
-        if 65 < ord(elements.upper()) < 90 or 192 < ord(elements.upper()) < 223:
-            return True
-    return False
+    for element in stringList:
+        if not 65 <= ord(element.upper()) <= 90 and not 1040 <= ord(element.upper()) <= 1071:
+            return False
+    return True
+    
 
 def CyrillicOrLatin(string):       # метод проверяет, состоит ли слово из букв одного алфавита
     stringList = list(string)
     newlist = sorted(list(word.upper()))
-    if 65 < ord(newlist[0]) < 90 and 192 < ord(newlist[len(newlist) - 1]) < 223:
+    if 65 <= ord(newlist[0]) <= 90 and 1040 <= ord(newlist[len(newlist) - 1]) <= 1071:
         return False
     return True
 
 word = input('Введите слово, написанное латиницей или кириллицей: ')
-if WordOrNot(word):
+
+if not WordOrNot(word):
     print('Вы ввели не слово.')
-elif CyrillicOrLatin(word):
+elif not CyrillicOrLatin(word):
     print('Ваше слово состоит из букв, взятых из различных алфавитов.')
 else:
-    print('отлично')
-
-# k = int(input())
-# if k in lettersDictionary.keys:
-#     spisok = list(lettersDictionary[k])
-# print(spisok)
-    
-
-# lettersToNumbers = []
-# for letters in wordList:
-#     for k in lettersDictionary.keys():
-#          if letters.upper() in k:
-#               lettersToNumbers.append(lettersDictionary[k])
-
-# print(sum(lettersToNumbers))
+    lettersToNumbers = []
+    for letter in list(word):
+        for key in lettersDictionary.keys():
+            if letter.upper() in key:
+                lettersToNumbers.append(lettersDictionary[key])
+    print(f'Стоимость введенного вами слова: {sum(lettersToNumbers)}')
